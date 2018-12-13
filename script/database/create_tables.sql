ff431409114f6e46b78014eb5c0cff432037b030
Lucas Concato <lu_cas.concato@hotmail.com>
2018-12-13 09:19:32 -0200

    Update create_tables.sql

--- script/database/create_tables.sql
+++ script/database/create_tables.sql
@@ -1,3 +1,15 @@
+-- Database: store
+ -- DROP DATABASE store;
+ 
+ CREATE DATABASE store
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
 CREATE TABLE public.fornecedor
 (
     id integer NOT NULL,
